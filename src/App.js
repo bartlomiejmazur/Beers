cebe84bf9a74c10877def1f4aa3c51856f449879
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-29 12:37:06 +0200

    🛠️ import all components to App.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import Nav from './components/Nav'
+
+import '../src/styles/App.css'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <Header />
+      <Main />
+      <Nav />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
